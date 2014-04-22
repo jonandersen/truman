3def723b2c3300691381640c3a883d0a3ba4c0b5
Jon Andersen <jon.andersen88@gmail.com>
2014-04-22 16:51:46 -0700

    Added file to git

---
+++ WanderlustTests/SightViewCellSpec.m
@@ -0,0 +1,38 @@
+#import <XCTest/XCTest.h>
+
+#import "Specta.h"
+
+
+#define EXP_SHORTHAND
+
+#import "Expecta.h"
+#import "SightViewCell.h"
+#import "SightViewController.h"
+
+SpecBegin(SightViewCellSpec)
+
+describe(@"SightViewCell", ^{
+    __block SightViewCell *sut;
+
+    beforeEach(^{
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle:nil];
+        SightViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"SightViewController"];
+        [vc view];
+       sut =  [vc.tableView dequeueReusableCellWithIdentifier:SightCellIdentifier];
+    });
+
+    it(@"should have an image view", ^{
+        expect(sut.sightImageView).toNot.beNil();
+    });
+
+    it(@"should have a location title", ^{
+        expect(sut.titleLabel).toNot.beNil();
+    });
+
+    it(@"should have height same as its frame", ^{
+        expect([sut height]).to.equal(sut.frame.size.height);
+    });
+
+});
+
+SpecEnd
